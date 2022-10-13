964caed3c5da01df267b145417a7e9f5f0b3bd6e
weix0015 <weix0015@stud.kea.dk>
2022-10-13 13:13:41 +0200

    Ans

---
+++ src/Ans.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Ans {
+        public static void main(String[] args){
+            Scanner s = new Scanner(System.in);
+            String a = s.next();
+            System.out.println(a);
+    }
+}
